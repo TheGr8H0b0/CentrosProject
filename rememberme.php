1acc574c18f7aa74ce84a66ee1b98f738117eb4a
Josh Worley <45806673+joshworley@users.noreply.github.com>
2019-11-19 00:13:28 -0500

    login functionality in index

--- rememberme.php
+++ rememberme.php
@@ -1,1 +1,13 @@
-hi
+<?php
+// Case 3: logout
+// delete the cookie by setting expiration date to the past
+//setcookie('user', '', time() - 3600);
+
+if( isset($_COOKIE['user']) ){
+    echo '<a class="nav-link" href="account.html"><i class="fas fa-user"></i> My Account</a>';
+}
+else {
+    echo '<a class="nav-link" href="login.html"><i class="fas fa-user"></i> Log in</a>';
+}
+  
+?>
