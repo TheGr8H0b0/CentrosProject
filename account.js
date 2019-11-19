216d84b7c6c4c37789612c9f0fb7301716f95c31
Josh Worley <45806673+joshworley@users.noreply.github.com>
2019-11-19 00:55:14 -0500

    account info displayed

--- account.js
+++ account.js
@@ -1,1 +1,21 @@
-hi
+$(document).ready(function(){
+        
+    var url = "account.php";
+    
+    // serialize packages the input values in the form
+    var jqxhr = $.post(url);
+    
+    // set up callbacks
+    jqxhr.done(function(data){
+        $("#account-info").html(data);
+    });
+    
+    jqxhr.fail(function(jqXHR){
+        console.log("Error: " + jqXHR.status);
+    });
+    
+    jqxhr.always(function(){
+        console.log("Done with AJAX request.");
+    });
+
+});
--- rememberme.php
+++ rememberme.php
@@ -1,7 +1,4 @@
 <?php
-// Case 3: logout
-// delete the cookie by setting expiration date to the past
-//setcookie('user', '', time() - 3600);
 
 if( isset($_COOKIE['user']) ){
     echo '<a class="nav-link" href="account.html"><i class="fas fa-user"></i> My Account</a>';
