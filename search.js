3239dfd696737566d2a85fd328411c2712d2ac40
leinad99 <37079904+leinad99@users.noreply.github.com>
2019-11-19 00:01:10 -0500

    search transformation on focus

--- search.js
+++ search.js
@@ -13,6 +13,15 @@ $(document).ready(function() {
 
         }
     });
+    $("input").focus(function(){
+        $("input").css("transition", "transform .5s, opacity .25s;");
+        $("input").css("transform", "scale(1)");
+        
+    });
+    $("input").blur(function(){
+        $("input").css("transition", "transform .5s, opacity .25s");
+        $("input").css("transform", "scale(.993,.94)");
+    });
 });
 
 function createResultDisplay(item, index, arr) {
