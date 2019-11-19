336eda71343b1f93d56b15c6f2e3e8cc2d88fedd
Josh Worley <45806673+joshworley@users.noreply.github.com>
2019-11-19 01:09:04 -0500

    login functionality

--- account.js
+++ account.js
@@ -18,4 +18,30 @@ $(document).ready(function(){
         console.log("Done with AJAX request.");
     });
 
+
+    $("#login-btn").on("click", function(e){
+        e.preventDefault();
+        
+        var url = "logout.php";
+        
+        // serialize packages the input values in the form
+        var data = $("form").serialize();
+        var jqxhr = $.post(url, data);
+        
+        // set up callbacks
+        jqxhr.done(function(data){
+            $("#response").html(data);
+        });
+        
+        jqxhr.fail(function(jqXHR){
+            console.log("Error: " + jqXHR.status);
+        });
+        
+        jqxhr.always(function(){
+            console.log("Done with AJAX request.");
+        });
+
+    });
+
+
 });
--- account.php
+++ account.php
@@ -3,7 +3,7 @@
 // delete the cookie by setting expiration date to the past
 //setcookie('user', '', time() - 3600);
 
-if( isset($_COOKIE['user']) ){
+if( isset($_COOKIE['user']) ) {
     // Start the DB operations
     $servername = "127.0.0.1";
     $username = "root";
@@ -33,15 +33,16 @@ if( isset($_COOKIE['user']) ){
         $stmt->close();
 
         if($usertype == 'regular'){
-            echo("<div class='userInfo' id='userEmail'>Email: $_COOKIE['user']</div>
+            echo("<div class='userInfo' id='userEmail'>Email: " . $_COOKIE["user"] . "</div>
             <div class='userInfo' id='numFavs'>Max Favorites: 5</div>
-            <div class='advertisement'>Non premium user. Click <a href='upgrade.html'>here</a> to upgrade!</div>");
+            <div class='userInfo'>Non premium user. Click <a href='upgrade.html'>here</a> to upgrade!</div>");
         }
         else {
-            echo("<div class='userInfo' id='userEmail'>Email: $_COOKIE['user']</div>
+            echo("<div class='userInfo' id='userEmail'>Email: " . $_COOKIE["user"] . "</div>
             <div class='userInfo' id='numFavs'>Max Favorites: Unlimited</div>
-            <div class='advertisement'>Premium user. Thank you for supporting Centros!</div>");
+            <div class='userInfo'>Premium user. Thank you for supporting Centros!</div>");
         }
+    }
 }
 else {
     echo("I'm not sure how you got here, hacker :P");
