26d0d2589fc27fbde914558d83825ea9cbd88076
Josh Worley <45806673+joshworley@users.noreply.github.com>
2019-12-08 23:03:42 -0500

    Upgrade page for logged off users

--- upgrade.js
+++ upgrade.js
@@ -1,1 +1,21 @@
-hi
+$(document).ready(function(){
+        
+    var url = "upgrade.php";
+    
+    // serialize packages the input values in the form
+    var jqxhr = $.post(url);
+    
+    // set up callbacks
+    jqxhr.done(function(data){
+        $("#upgrade-results").html(data);
+    });
+    
+    jqxhr.fail(function(jqXHR){
+        console.log("Error: " + jqXHR.status);
+    });
+    
+    jqxhr.always(function(){
+        console.log("Done with AJAX request.");
+    });
+
+});
--- upgrade.php
+++ upgrade.php
@@ -1,1 +1,21 @@
-hi
+<?php
+
+if( isset($_COOKIE['user']) ) { // The User is Logged In
+    if( isset($_COOKIE['premium']) ) { // The Logged in User is a PREMIUM User
+        echo '';
+    }
+    else { // The Logged in User is NOT a Premium User
+        echo '';
+    }
+}
+else { // The User is NOT Logged In
+    echo '<div class="upgrade-txt">You are not currently logged in. Log in or Sign Up now to upgrade!</div>
+    <div class="center-button">
+      <a href="login.html"><button type="button">Log In</button></a>
+    </div>
+    <div class="center-button">
+      <a href="signup.html"><button type="button">Sign Up</button></a>
+    </div>';
+}
+  
+?>
