ff67785bc686ab9ecdc6355b5c695ac70e2102bf
Josh Worley <45806673+joshworley@users.noreply.github.com>
2019-12-08 23:31:25 -0500

    Upgrade changes

--- upgrade.php
+++ upgrade.php
@@ -5,7 +5,7 @@ if( isset($_COOKIE['user']) ) { // The User is Logged In
         echo '<h2 class="sub-header">You are a premium user. Thank you for your support!<br>Enjoy your unlimited favorites and ad-free exploration of our site!</h2>';
     }
     else { // The Logged in User is NOT a Premium User
-        echo '<h2 class="sub=header">Upgrade now!<br>No more ads!<br>Unlimited Favorites!<br>Support Centros!</h2>
+        echo '<h2 class="sub-header">Upgrade now!<br>No more ads!<br>Unlimited Favorites!<br>Support Centros!</h2>
         <div class="button-container">
             <a href="payment.html" class="btn"><span>Pay Now</span></a>
         </div>';
