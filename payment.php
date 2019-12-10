0a868c59bb3e486f8bf11e9c725a875aa33d963c
Josh Worley <45806673+joshworley@users.noreply.github.com>
2019-12-09 21:46:06 -0500

    Payment page makes user premium

--- payment.php
+++ payment.php
@@ -1,10 +1,50 @@
 <?php
+$statusMessage = "";
 
-if( isset($_COOKIE['user'] && isset($_COOKIE['premium']) ) { // The User can actually pay
-    
-}
-else {
+// Since this request will alter the DB, we use POST instead of GET
+if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
+    // Check the fields are present
+    if(isset($_COOKIE["user"])) {
+        
+        $email = $_COOKIE["user"];
 
+        // Start the DB operations
+        $servername = "127.0.0.1";
+        $username = "root";
+        $password = "";
+        $dbname = "centros";
+                    
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_errno) {
+            $statusMessage = "Could not connect to database";
+        }
+        else {
+            // Get the information for the relevant accounts
+            $stmt = $conn->prepare("UPDATE `clients` SET `usertype`='premium' WHERE username=?");
+            
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            
+            $stmt->close();
+
+            $statusMessage = "You are now a premium member!";
+        }
+        
+        $conn->close();
+        
+    }
+    else{
+        $statusMessage = "You are not logged in";
+    }
 }
-  
+else{
+    $statusMessage = "No POST request received.";
+}
+
+// Echo the result        
+echo($statusMessage);
+
 ?>
