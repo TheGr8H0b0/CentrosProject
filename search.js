65a261ead72d03ccde1f991a9650ac8c3d68cc48
leinad99 <37079904+leinad99@users.noreply.github.com>
2019-12-11 20:40:47 -0500

    animate star?

--- search.js
+++ search.js
@@ -269,59 +269,20 @@ function loadSearchResults() {
     }
     // Favorites click Listener code
     $(".fav-star").on("click", function() {
-        var title = $(this).parent().find(".item-title").text();
-        var price = $(this).parent().find(".price").text();
-        var itemLink = $(this).parent().find(".item-link").attr("href");
-        var imgLink = $(this).parent().find(".col-xs-3 img").attr("src");
-        console.log("Title: " + title + " Price: " + price + " Item Link: " + itemLink + " Image Link: " + imgLink);
-
-        //Set cookie values for 1 minute to transfer values to addFavorite.php
-        var cookieTTLMinutes = "1";
-        createCookie("title",title,cookieTTLMinutes);
-        createCookie("price",price,cookieTTLMinutes);
-        createCookie("itemLink",itemLink,cookieTTLMinutes);
-        createCookie("imgLink",imgLink,cookieTTLMinutes);
-
-        //Make an ajax post to addFavorite.php so it can add this item to the database
-        var url = "addFavorite.php";
-        
-        var jqxhr = $.post(url);
-        var textItem = $(this);
-        
-        // set up callbacks
-        jqxhr.done(function(data){
-            //Use the response to the ajax post to give feedback
-            console.log(data);
-            textItem.text(String(data));
-        });
-        
-        jqxhr.fail(function(jqXHR){
-            console.log("Error: " + jqXHR.status);
-        });
-        
-        jqxhr.always(function(){
-            //Within ajax.always, delete the cookie
-            console.log("Done with AJAX request.");
-        });
-
-
+        $(this).children().addClass("rotate"); //expirimental
+        if($(this).children().hasClass("fa")){
+            $(this).children().removeClass("fa");
+        }  
+        else{ //file in star and add to favorites
+            $(this).children().addClass("fa");
+             // You'll need to test all of these, some may work, some may not
+            var title = $(this).parent().find(".item-title").text();
+            var price = $(this).parent().find(".price").text();
+            var itemLink = $(this).parent().find(".item-link").attr("href");
+            var imgLink = $(this).parent().find(".col-xs-3 img").attr("src");
+            console.log("Title: " + title + " Price: " + price + " Item Link: " + itemLink + " Image Link: " + imgLink);
+        }
     });
-}
-
-// Function to create a cookie (to be passed to addFavorite.php)
-function createCookie(name, value, minutes) { 
-    var expires; 
-      
-    if (minutes) { 
-        var date = new Date(); 
-        date.setTime(date.getTime() + (minutes * 60 * 1000)); 
-        expires = "; expires=" + date.toGMTString(); 
-    } 
-    else { 
-        expires = ""; 
-    } 
-      
-    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/"; 
 }
 
 function createResultDisplay(item) {
@@ -347,13 +308,14 @@ function createResultDisplay(item) {
                         "</div>" + 
                     "</div>" + 
                 "</a>" +
-                "<div class='fav-star'>Click to Favorite</div>" +
+                "<div class='fav-star'>STAR</div>" +
             "</div>";
 
             $("#search-results").append(htmlAppend);
         } else if (item != undefined) {
             var htmlAppend =
             "<div class='item-container'>" +
+                "<div class='fav-star'><i class='far fa-star fav-star-icon'></i></div>" +
                 "<a class='item-link' target='_blank' href=" + "https://www.google.com/" + item.link + ">" +
                     "<div class='row'>" + 
                         "<div class='col-xs-3'>" +
@@ -372,7 +334,6 @@ function createResultDisplay(item) {
                         "</div>" + 
                     "</div>" + 
                 "</a>" +
-                "<div class='fav-star'>Click to Favorite</div>" +
             "</div>";
 
             $("#search-results").append(htmlAppend);
