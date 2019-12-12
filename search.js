1f4c20055fb1a9c179034d3054136cb9ec45ad45
leinad99 <37079904+leinad99@users.noreply.github.com>
2019-12-11 21:10:21 -0500

    merge

--- search.js
+++ search.js
@@ -269,6 +269,43 @@ function loadSearchResults() {
     }
     // Favorites click Listener code
     $(".fav-star").on("click", function() {
+        var title = $(this).parent().find(".item-title").text();
+        var price = $(this).parent().find(".price").text();
+        var itemLink = $(this).parent().find(".item-link").attr("href");
+        var imgLink = $(this).parent().find(".col-xs-3 img").attr("src");
+        console.log("Title: " + title + " Price: " + price + " Item Link: " + itemLink + " Image Link: " + imgLink);
+
+        //Set cookie values for 1 minute to transfer values to addFavorite.php
+        var cookieTTLMinutes = "1";
+        createCookie("title",title,cookieTTLMinutes);
+        createCookie("price",price,cookieTTLMinutes);
+        createCookie("itemLink",itemLink,cookieTTLMinutes);
+        createCookie("imgLink",imgLink,cookieTTLMinutes);
+
+        //Make an ajax post to addFavorite.php so it can add this item to the database
+        var url = "addFavorite.php";
+        
+        var jqxhr = $.post(url);
+        var textItem = $(this);
+        
+        // set up callbacks
+        jqxhr.done(function(data){
+            //Use the response to the ajax post to give feedback
+            console.log(data);
+            textItem.text(String(data));
+        });
+        
+        jqxhr.fail(function(jqXHR){
+            console.log("Error: " + jqXHR.status);
+        });
+        
+        jqxhr.always(function(){
+            //Within ajax.always, delete the cookie
+            console.log("Done with AJAX request.");
+        });
+
+
+        //Animate star 
         if($(this).children().hasClass("fa")){
             $(this).children().removeClass("rotate"); 
             $(this).children().removeClass("fa");
@@ -285,7 +322,20 @@ function loadSearchResults() {
         }
     });
 }
-
+function createCookie(name, value, minutes) { 
+    var expires; 
+      
+    if (minutes) { 
+        var date = new Date(); 
+        date.setTime(date.getTime() + (minutes * 60 * 1000)); 
+        expires = "; expires=" + date.toGMTString(); 
+    } 
+    else { 
+        expires = ""; 
+    } 
+      
+    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/"; 
+}
 function createResultDisplay(item) {
     if (item != undefined && item.title != null && item.price != null && item.link && item.thumbnail) {
         if (item.description == null) {
